71dc7a612f8e7d42996f2819d011b5c32f812e66
Adam Rackis <adamrackis@hotmail.com>
2022-10-30 14:52:11 -0500

    start react query work

--- app/Todos.tsx
+++ app/Todos.tsx
@@ -1,23 +1,37 @@
 "use client";
 
-import { FC } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { FC, useState } from "react";
 import type { TodosResult } from "../types";
 
 export const Todos: FC<{ todos: TodosResult }> = (props) => {
+  const [filter, setFilter] = useState("");
+
+  const xxx = useQuery<TodosResult>(
+    ["todos"],
+    async () => {
+      const res = await fetch("api/todos");
+      return res.json();
+    },
+    {
+      initialData: { data: [{ name: "", priority: "high" }] },
+    }
+  );
+
   return (
     <section>
       Current Todos
       <hr />
       <span>Filter:</span>
-      <select>
+      <select value={filter} onChange={(evt) => setFilter(evt.target.value)}>
         <option value="">All</option>
         <option value="high">High</option>
         <option value="medium">Medium</option>
         <option value="low">Low</option>
       </select>
       <ul>
-        {props.todos.data.map((todo) => (
-          <li>
+        {props.todos.data.map((todo, idx) => (
+          <li key={idx}>
             {todo.name} - {todo.priority}
           </li>
         ))}
